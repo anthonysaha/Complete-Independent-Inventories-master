d70caaf98703f512af07352567346782728041b1
anthonysaha <asaha3@yahoo.com>
2019-04-22 17:25:18 +0100

    replace contact form

--- assets/partials/header.php
+++ assets/partials/header.php
@@ -21,6 +21,8 @@
         <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
         <!-- Google Fonts -->    
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
+        <!-- Google recaptcha API library -->
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
         <script src="assets/js/html5shiv.js"></script>
